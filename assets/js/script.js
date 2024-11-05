9bafd864735c1599c825c449b4a11399075aaa81
Roeich <roeichsheikh@gmail.com>
2024-11-05 17:02:21 +0600

    initial

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+    /* __________________ start home page __________________ */
     // ... header marquee
     $('.header_marquee .marquee').marquee({
         startVisible: true,
@@ -19,5 +19,19 @@ $(document).ready(function() {
         margin:10,
         autoplay:true,
         autoplayTimeout: 3000
-    })
+    });
+
+    // .. filtering items
+    $("[data-filter]").on("click",function(){
+        const selector=$(this).attr("data-filter");
+        $("[data-filter]").removeClass("active");
+        $(this).addClass("active");
+        if(selector==="*"){
+            $(".filter_item").removeClass("d-none");
+        }else{
+            $(".filter_item").addClass("d-none");
+            $(`.${selector}`).removeClass("d-none");
+        }
+    });
+    /* __________________ end home page __________________ */
 });
